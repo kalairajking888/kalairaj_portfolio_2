ad119e1c5e16aa459abb552ce0064230b363bd14
Kalairaj <128981568+kalairajking888@users.noreply.github.com>
2024-06-15 16:14:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const changeTextButton = document.getElementById("changeTextButton");
-    const dynamicText = document.getElementById("dynamicText");
-
-    changeTextButton.addEventListener("click", function() {
-        dynamicText.textContent = "The text has been changed!";
-    });
-});
